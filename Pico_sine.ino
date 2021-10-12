752f67dd2047771fb239514cd2fcb8261fd0f407
zsoltibaba37 <petozsolt33@gmail.com>
2021-10-12 16:19:27 +0200

    Add new design :-)

--- Pico_sine.ino
+++ Pico_sine.ino
@@ -3,7 +3,7 @@
  */
 
 
-float version = 0.01;
+float version = 0.1;
 
 #define LED 25
 
@@ -47,6 +47,7 @@ void setup() {
   }
 
   // ------------------ Potentiometer ------------------  
+  pinMode(LED, OUTPUT);
   pinMode(SETPOINT_PIN, INPUT);
 
   // ------------------ INITIAL DISPLAY  ------------------
@@ -64,7 +65,7 @@ void setup() {
   display.println("v" + String(version));
   display.drawRect(0, 0, 128, 64, SSD1306_WHITE);
   display.display();
-  delay(4000);
+  delay(500);
   
 }
 
@@ -74,10 +75,13 @@ void loop() {
 
   drawBorder();
   float a = 0.0;
-
+  int countC = 0;
+  
   //------------- Read Poti ------------------
   valuePoti = float(analogRead(SETPOINT_PIN));
   Setpoint = mapfloat(valuePoti, 0.0, 1023.0, 2.0, 63.0);
+  int valuePot = analogRead(SETPOINT_PIN);
+  int setLed = map(valuePot, 10, 1023, 0, 255);
   //Serial.println(Setpoint);
 
   float Mod = 64.0 - Setpoint;
@@ -89,16 +93,30 @@ void loop() {
     float mapY = mapfloat(sina, frommin, frommax, tomin, tomax);
     display.drawPixel(i, int(mapY), SSD1306_WHITE);
     a = a + incr;
+    if(countC == 32){
+      display.drawCircle(i, 32, 2, SSD1306_WHITE);
+      countC = 0;
+    }
+    countC += 1;
   }
-  display.setTextSize(2);
+  display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(90, 2);
-  display.print("amp");
+  int texty ;
+  if(tomin < 30){
+    texty = 54;
+  }
+  else{
+    texty = 2;
+  }
+  display.setCursor(100, texty);
+  display.print("sine");
   display.display();
-  //delay(1);
   display.clearDisplay();
+  analogWrite(LED, setLed);
 
 }
+// ------------------ END LOOP ------------------
+// ------------------ END LOOP ------------------
 
 // ------------------ VOIDS ------------------
 // ------------------ VOIDS ------------------
